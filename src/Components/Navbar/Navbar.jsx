5f1acf9e13cd97ab01852c95e99ecabd8968d6dc
Syed Anas Ahmed <syedanasahmed412@gmail.com>
2024-04-24 01:53:51 +0500

    home page complete

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,8 +2,7 @@ import { NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import React, { useState } from 'react';
 import { HiBars3 } from "react-icons/hi2";
-
-
+import { RxCross1 } from "react-icons/rx";
 
 function MyNavbar() {
     const [collapsed, setCollapsed] = useState(true);
@@ -12,13 +11,15 @@ function MyNavbar() {
         setCollapsed(!collapsed);
     };
 
+
     return (
         <>
             <div className={`${styles.customnav} `}>
                 <ul className={`${styles.mynavlinks} `}>
                     <ul className={styles.logoholder}>
-                        <img src="/public/Images/Logo.png"  width={79} height={46} alt="" />
+                        <img src="/public/Images/Logo.png" width={79} height={46} alt="" />
                     </ul>
+
                     <li className={`${styles.homepagelink}`}>
                         <NavLink to="/" >HOME</NavLink>
                     </li>
@@ -35,12 +36,24 @@ function MyNavbar() {
                         <NavLink to="/Contact" >CONTACT US</NavLink>
                     </li>
                 </ul>
-                {/* <button className={`${styles.collapseButton}`} onClick={toggleCollapse}>
-                    {collapsed ? <HiBars3 />  : 'Collapse'}
-                </button> */}
+                <button className={`${styles.collapseButton}`} onClick={toggleCollapse}>
+                    {collapsed ? <HiBars3 />  : <RxCross1 />}
+                </button>
             </div>
         </>
     )
 }
 
 export default MyNavbar
+
+
+ {/* <li className={styles.dropdown}>
+                        <NavLink to="#" className={styles.dropbtn}>MENU</NavLink>
+                        <div className={`${styles.dropdownContent}`}>
+                            <NavLink to="/" >HOME</NavLink>
+                            <NavLink to="/Schedule" >SCHEDULE OF EVENTS</NavLink>
+                            <NavLink to="/Cast" >CAST ATTENDEES</NavLink>
+                            <NavLink to="/Tickets" >TICKETS</NavLink>
+                            <NavLink to="/Contact" >CONTACT US</NavLink>
+                        </div>
+                    </li> */}
